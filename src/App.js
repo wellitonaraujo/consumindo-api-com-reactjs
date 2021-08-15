29bdfd05e260e5037f4116a290ce42c8a9587c6b
wellitonaraujo <wellitonaraujodev@hotmail.com>
2021-08-15 17:11:33 -0300

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+import './style.css';
 
 function App() {
+  const [ post, setPost ] = useState([]) // os Hooks da aplicação
+
+  // useEffect - função que será chamada quando  a págin for carregada
+  useEffect(() => {
+    async function loadApi() { // função da requisição com a FETCH API
+      const resp = await fetch('https://sujeitoprogramador.com/rn-api/?api=posts');
+      const json = await resp.json();
+      setPost(json)
+    }
+
+    loadApi(); // invocando a função da requisição
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
       </header>
+
+      {post.map((item) => { // mapeando os itens da API
+        return(
+          <section className="post" key={item.id}> 
+            <strong className="titulo">{item.titulo}</strong>
+            <img className="capa" src={item.capa}></img>
+            <p className="subtitulo">{item.subtitulo}</p>
+            <a className="botao">Acessar</a>
+          </section>
+        )
+      })}    
     </div>
   );
 }
-
 export default App;
